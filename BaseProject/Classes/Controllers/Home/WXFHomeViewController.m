ebf1c38d65dccbc848478f169e790cddcb1c7d72
wuxiaofang <wuxiaofang@yongche.com>
2016-10-23 15:26:32 +0800

    refs #23468, 首页调整间距

--- BaseProject/Classes/Controllers/Home/WXFHomeViewController.m
+++ BaseProject/Classes/Controllers/Home/WXFHomeViewController.m
@@ -284,7 +284,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 74;
+    return 84;
 }
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
